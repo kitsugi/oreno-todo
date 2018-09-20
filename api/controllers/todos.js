c4af6ad9ef1974c758f7af36a06715b2c938857b
kitsugi <kyoichi.kitsugi@gmail.com>
2018-09-21 06:15:07 +0900

    500エラー対応

--- api/controllers/todos.js
+++ api/controllers/todos.js
@@ -18,9 +18,10 @@ function getTodoList (req, res) {
     done = (status === 'done')
   }
 
-  dao.search(keyword, done, (err, docs) => {
-    console.log(err, docs)
+  dao.search(keyword, done).then(docs => {
     res.json(docs)
+  }, err => {
+    res.status(500).json(new Error(err.code, err.message))
   })
 }
 
@@ -33,13 +34,14 @@ function getTodoList (req, res) {
 function getTodoById (req, res) {
   const todoId = req.swagger.params.id.value
 
-  dao.getById(todoId, (err, doc) => {
-    console.log(err, doc)
+  dao.getById(todoId).then(doc => {
     if (doc) {
       res.status(200).json(doc)
     } else {
       res.status(404).json(new Error('404', 'A todo with the specified ID was not found.'))
     }
+  }, err => {
+    res.status(500).json(new Error(err.code, err.message))
   })
 }
 
@@ -53,9 +55,10 @@ function createTodo (req, res) {
   const entry = req.swagger.params.entry.value
 
   if (entry.title) {
-    dao.create(entry, (err, newDoc) => {
-      console.log(err, newDoc)
+    dao.create(entry).then(newDoc => {
       res.status(201).json(newDoc)
+    }, err => {
+      res.status(500).json(new Error(err.code, err.message))
     })
   } else {
     res.status(400).json(new Error('400', 'title empty'))
@@ -72,13 +75,14 @@ function updateTodo (req, res) {
   const todoId = req.swagger.params.id.value
   const entry = req.swagger.params.entry.value
 
-  dao.update(todoId, entry, (err, numAffected) => {
-    console.log(err, numAffected)
+  dao.update(todoId, entry).then(numAffected => {
     if (numAffected > 0) {
       getTodoById(req, res)
     } else {
       res.status(404).json(new Error('404', 'A todo with the specified ID was not found.'))
     }
+  }, err => {
+    res.status(500).json(new Error(err.code, err.message))
   })
 }
 
@@ -91,13 +95,14 @@ function updateTodo (req, res) {
 function deleteTodo (req, res) {
   const todoId = req.swagger.params.id.value
 
-  dao.delete(todoId, (err, numRemoved) => {
-    console.log(err, numRemoved)
+  dao.delete(todoId).then(numRemoved => {
     if (numRemoved > 0) {
       res.status(204).end()
     } else {
       res.status(404).json(new Error('404', 'A todo with the specified ID was not found.'))
     }
+  }, err => {
+    res.status(500).json(new Error(err.code, err.message))
   })
 }
 
